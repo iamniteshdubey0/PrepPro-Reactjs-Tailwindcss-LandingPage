ff87bb13ce3fc585085383e9cf7ad46910f32b6c
Nitesh <dubeynit333@gmail.com>
2025-06-25 20:37:38 +0530

    feat: add background grid effect to WhyUs component for enhanced visual appeal

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -5,6 +5,22 @@ const WhyUs = () => {
   return (
     <section id="whyus" className="p-2 md:py-8 md:px-4 relative">
       <div className="container mx-auto space-y-24">
+        {/* background grid effect */}
+        <div
+          className="absolute inset-0 z-0 max-sm:hidden"
+          style={{
+            backgroundImage: `
+      linear-gradient(to right, rgba(0,0,0,0.05) 1px, transparent 1px),
+      linear-gradient(to bottom, rgba(0,0,0,0.05) 1px, transparent 1px)
+    `,
+            backgroundSize: "100px 100px",
+            WebkitMaskImage:
+              "linear-gradient(to right, black 65%, transparent 50%)",
+            maskImage: "linear-gradient(to right, black 65%, transparent 50%)",
+            WebkitMaskRepeat: "no-repeat",
+            maskRepeat: "no-repeat",
+          }}
+        ></div>
         <div className="flex flex-col md:flex-row items-start md:my-8 md:py-8 space-y-4">
           <div className="md:flex-1/3">
             <p className="text-foreground/70 text-xl font-semibold capitalize">
