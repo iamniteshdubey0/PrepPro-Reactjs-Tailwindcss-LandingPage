f23de60e4cdb39def553406c12e2fb9a363f8720
Nitesh <dubeynit333@gmail.com>
2025-07-01 22:30:12 +0530

    feat: enhance Navbar with dropdown sublinks and improve layout for better user experience

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           {NavrbarItems.navlinks.map((item, key) => (
             <div key={key} className="relative inline-block text-left">
               <a
-              href={item.link}
+                href={item.link}
                 onClick={() => setActiveIndex(activeIndex === key ? null : key)}
                 className="flex justify-between gap-1 items-center text-sm font-medium text-foreground px-2 py-2 hover:text-primary transition-all duration-300 cursor-pointer"
               >
@@ -101,28 +101,72 @@ const Navbar = () => {
 
         <div
           className={cn(
-            "fixed h-screen inset-0 bg-background/95 backdrop-blur-md",
-            "z-40 flex flex-col justify-center items-center",
+            "absolute top-0 h-screen inset-0 bg-background px-10 pt-24",
+            "z-40 flex flex-col",
             "transition-all duration-300 md:hidden",
             isMenuOpened
               ? "opacity-100 pointer-events-auto"
               : "opacity-0 pointer-events-none"
           )}
         >
-          <div className="flex flex-col space-y-8 text-xl">
+          <div className="flex flex-col space-y-6 text-xl">
             {NavrbarItems.navlinks.map((item, key) => (
-              <a
-                key={key}
-                href={item.link}
-                className="font-semibold text-foreground/80 hover:text-primary transition-colors duration-300 capitalize"
-                onClick={() => setIsMenuOpened(false)}
-              >
-                {item.label}
-              </a>
+              <div key={key} className="relative inline-block text-left max-h-3/5 overflow-y-auto">
+                <a
+                  key={key}
+                  href={item.link}
+                  onClick={() =>
+                    setActiveIndex(activeIndex === key ? null : key)
+                  }
+                  className="flex gap-2 justify-start items-center text-2xl font-semibold text-foreground/80 hover:text-primary transition-colors duration-300 capitalize"
+                >
+                  {item.label}
+                  {item.sublinks && (
+                    <span className="transition-all duration-300">
+                      {activeIndex === key ? (
+                        <ChevronDown size={20} /> 
+                      ) : (
+                        <ChevronRight size={20} />
+                      )}
+                    </span>
+                  )}
+                </a>
+                {activeIndex === key && item.sublinks && (
+                  <div className="z-50 mt-2 max-h-3/4 overflow-y-auto w-full bg-background/80 border rounded-xl shadow-lg px-2 py-3 grid grid-cols-1 gap-2">
+                    {item.sublinks.map((subitem, i) => (
+                      <div
+                        key={i}
+                        className={`flex items-start gap-2 space-y-3 p-1 rounded-lg transition-all duration-300 cursor-pointer ${
+                          subitem.highlight
+                            ? "bg-primary/20 hover:bg-primary/30"
+                            : "hover:bg-primary/20"
+                        }`}
+                      >
+                        <div className="w-16 h-16 rounded-md bg-muted flex items-center justify-center">
+                          {subitem.icon}
+                        </div>
+                        <div>
+                          <p className="text-lg font-semibold flex items-center gap-1">
+                            {subitem.title}
+                            {subitem.badge && (
+                              <span className="text-md bg-blue-100 text-blue-600 px-1 py-0.5 rounded-full">
+                                {subitem.badge}
+                              </span>
+                            )}
+                          </p>
+                          <p className="text-md text-muted-foreground mt-0.5">
+                            {subitem.desc}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
             ))}
           </div>
 
-          <div className="fixed bottom-16 flex space-x-4">
+          <div className="absolute bottom-10 flex space-x-4">
             <button className="outlined-button text-md">Login</button>
             <button className="solid-button text-md">Request a Demo</button>
             <ToggleTheme></ToggleTheme>
